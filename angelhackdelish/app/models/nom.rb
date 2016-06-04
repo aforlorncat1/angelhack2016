6f922644f132e33397c51dcb3cb587f2210c41d9
Arnold <arnybarnie@gmail.com>
2016-06-05 00:22:22 +0800

    Added vote_count method to noms

--- angelhackdelish/app/models/nom.rb
+++ angelhackdelish/app/models/nom.rb
@@ -1,4 +1,10 @@
 class Nom < ActiveRecord::Base
 	belongs_to :food
 	belongs_to :restaurant
+  has_many :votes
+
+  def vote_count
+    self.votes.count 
+  end
+
 end
--- angelhackdelish/db/schema.rb
+++ angelhackdelish/db/schema.rb
@@ -11,35 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 20160604092314) do
-
+ActiveRecord::Schema.define(version: 20160604103933) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-
-  create_table "users", force: :cascade do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
-    t.string   "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.inet     "current_sign_in_ip"
-    t.inet     "last_sign_in_ip"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
+  create_table "authentications", force: :cascade do |t|
+    t.string   "user_id"
     t.string   "provider"
     t.string   "uid"
-    t.string   "name"
-    t.string   "image"
+    t.string   "token"
+    t.string   "token_secret"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
   end
-
-  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
   create_table "foods", force: :cascade do |t|
     t.string   "name"
@@ -66,4 +51,39 @@ ActiveRecord::Schema.define(version: 20160604092314) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "users", force: :cascade do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.inet     "current_sign_in_ip"
+    t.inet     "last_sign_in_ip"
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+    t.string   "provider"
+    t.string   "uid"
+    t.string   "name"
+    t.string   "image"
+  end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+
+  create_table "votes", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "nom_id"
+    t.integer  "vote_type"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "votes", ["nom_id"], name: "index_votes_on_nom_id", using: :btree
+  add_index "votes", ["user_id"], name: "index_votes_on_user_id", using: :btree
+
+  add_foreign_key "votes", "noms"
+  add_foreign_key "votes", "users"
 end
