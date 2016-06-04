360b4521d888c08b7c9d04398a99e6062b84503b
MedetaiAkaru <akaru@littlix.com>
2016-06-05 01:21:32 +0800

    seeds and votes

--- angelhackdelish/app/models/nom.rb
+++ angelhackdelish/app/models/nom.rb
@@ -1,4 +1,5 @@
 class Nom < ActiveRecord::Base
 	belongs_to :food
 	belongs_to :restaurant
+	has_many :votes
 end
--- angelhackdelish/app/models/vote.rb
+++ angelhackdelish/app/models/vote.rb
@@ -1,3 +1,8 @@
 class Vote < ActiveRecord::Base
   belongs_to :nom
+  enum vote_type: [:upvote, :downvote]
+
+  def self.votecounttwo(nom_id)
+  	Nom.find(nom_id).votes.upvote.count - Nom.find(nom_id).votes.downvote.count
+  end
 end
